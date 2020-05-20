54810cd06b89af6c77a7d6776f6872e46da00c94
Nate Buckareff <nate.buckareff@gmail.com>
2020-05-19 21:09:26 -0400

    Added Array

--- src/Array.ts
+++ src/Array.ts
@@ -1,6 +1,6 @@
 import { Parser, makeParser } from './Parser';
 
-function _Array<I, O>(parser: Parser<I, O>): Parser<I, O[]> {
+function _Array<T>(parser: Parser<unknown, T>): Parser<unknown, T[]> {
     return makeParser('Array', [parser], array => {
         if (Array.isArray(array)) {
             for (const x of array) {
